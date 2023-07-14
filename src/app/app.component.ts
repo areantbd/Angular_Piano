11bfd9746054cb9494e3d26cd20d263eb655465c
Iván <ivantbd87@gmail.com>
2023-07-14 17:33:32 +0200

    Piano done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Notes{
+  Do: string,
+  Re: string,
+  Mi: string,
+  Fa: string,
+  Sol: string,
+  La: string,
+  Si: string,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Piano';
+
+  aplicarSonido(nota: string): void {
+    const audio = new Audio()
+    audio.src = `../assets/sonidos/${nota}.wav`
+    audio.load()
+    audio.play()
+  }
 }
